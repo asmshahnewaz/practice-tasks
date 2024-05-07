8b86f012c3c9cdb86ceaec969a8a9647c650693a
asm shahnewaz <asmshahnewaaz@gmail.com>
2024-05-07 13:51:35 +0600

    js practice task

---
+++ task-4.js
@@ -0,0 +1,17 @@
+// John's teacher gave him two variables. Each variable contains a string. John's teacher asked him to combine these two strings and print them in one line. Help John write the program.
+// Input:
+// The first and the second lines of the input contain the strings.
+// Output:
+// Print the result in one line.
+// Sample Input:
+// I am going to be
+// an awesome web developer
+// Output:
+// I am going to be an awesome web developer
+
+var firstLine = "I am going to be";
+var secondLine = "an awesome web developer";
+
+var result = firstLine + " " + secondLine;
+
+console.log(result);
